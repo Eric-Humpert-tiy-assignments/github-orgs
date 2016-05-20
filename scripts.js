c54ec7b38fad08e0c8aebcac2a224d7de3a19404
Thwipshot <ethumpert@gmail.com>
2016-05-20 11:41:35 -0500

    updated some element tags and styling and restructured the javascript

--- scripts.js
+++ scripts.js
@@ -3,9 +3,24 @@ if (this.OrgsView === undefined) this.OrgsView = {};
 
 (function(context) {
 
+  var apiData;
+
+  function buildDOMList(obj) {
+
+  }
+
+  var organizationsList = document.querySelector('#org-list');
+
   function start() {
 
-    $.ajax('https://api.github.com/users/jisaacks/orgs');
+    $.ajax('https://api.github.com/users/jisaacks/orgs').done(function(result) {
+      console.log('got the data', result);
+      apiData = result;
+      console.log(apiData);
+      buildDOMList(apiData);
+    });
+
+
     //Call your code here
     console.log('starting!', context);
 
